02a204b86256fac974d4ddb5666acca23563fdec
lusayo-nyondo <lusayohendy@gmail.com>
2025-07-20 02:59:50 +0200

    Interesting output.

--- data-types.c
+++ data-types.c
@@ -4,11 +4,17 @@
  * int  4 bytes - %d
  * float 4 bytes - %f
  * char 1 byte - %c
- * double 8 bytes - %lf
+ * double 8 bytes - %lf - has 6 digits after decimal point.
  */
 
 int main() {
     int age = 10;
     double height = 100.4;
+    float epoch = 200.3;
+    double epochDouble = 200.3;
+
     printf("Age: %d, Height: %.2lf\n", age, height);
+    printf("Height: %f\n", height);
+    printf("Float: %f\n", epoch);
+    printf("Double: %lf\n", epochDouble);
 }
